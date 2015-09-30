7d319a20355d17e5535135207164ef51a674e9f1
RaibhanS <Raibhan.sasane@gmail.com>
2015-09-30 10:25:43 +0530

    Added Server Folder structure

---
+++ Server/app/routes/hospitals.js
@@ -0,0 +1,203 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+var hospitalModel = mongoose.model('Hospital', {name: String, address: String});
+
+var dbServer = 'mongodb://localhost:27017/QAWeb';
+var statusOk = {status:'ok', detail :''};
+var statusError =  {status :'500', detail :''};
+
+
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose default connection open to ' + dbServer);
+});
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {
+    console.log('Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', function() {
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+    });
+});
+
+
+// CLASS
+var clsHospital  = {
+    connectDB : function(){
+        mongoose.connect(dbServer,function(error){
+            if(error){
+                console.log(error);
+            }
+        });
+    },
+    disconnectDB :function(){
+
+    },
+    responseHandler : function (err, hospital){
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('saved successfully:', hospital);
+        }
+    },
+    addDummy : function(res){
+        this.connectDB();
+        var hospital1 = new hospitalModel({name: 'Hospital1', address :'Mumbai, India'});
+        var hospital2 = new hospitalModel({name: 'Hospital2', address :'Pune, India'});
+        var hospital3 = new hospitalModel({name: 'Hospital3', address :'Bangalore, India'});
+
+        hospital1.save(function (err, hospital) {
+            clsHospital.responseHandler(err,hospital);
+        });
+        hospital2.save(function (err, hospital) {
+            clsHospital.responseHandler(err,hospital);
+        });
+        hospital3.save(function (err, hospital) {
+            clsHospital.responseHandler(err,hospital);
+        });
+        this.disconnectDB();
+        res.json(statusOk);
+        res.end();
+        //mongoose.connection.close();
+    },
+    getAll : function(res){
+        console.log('GetAll executed');
+        this.connectDB();
+        hospitalModel.find({},function(err,docs){
+            console.log('dataFetch');
+            res.json(docs);
+            res.end();
+        });
+    },
+    getById : function(id,res, next){
+        console.log('Get by Name executed');
+        this.connectDB();
+        /*
+         hospitalModel.find({name:hospitalName},function(err,docs){
+         res.json(docs);
+         res.end();
+         });
+         */
+        hospitalModel.findById(id,
+            function (err, data) {
+                if (err) return next(err);
+                res.json(data);
+                res.end();
+            }
+        );
+    },
+    deleteById :function(id,res, next){
+        console.log('Delete by Name executed');
+        this.connectDB();
+        /*
+         hospitalModel.splice({name:hospitalName},function(err,docs){
+         res.json(statusOk);
+         res.end();
+         });
+         */
+        hospitalModel.findByIdAndRemove(id,
+            function (err, post) {
+                if (err) return next(err);
+                res.json(statusOk);
+                res.end();
+            }
+        );
+
+    },
+    add :function(hospital, res, next){
+        console.log('Add new Hospital');
+        if((hospital) && (hospital.name.length) && hospital.name.length > 0 && (hospital.address) && hospital.address.length > 0){
+            this.connectDB();
+            //hospitalModel.create(hospital);
+            hospitalModel.create(hospital,
+                function (err, post) {
+                    if (err) return next(err);
+                    res.json(statusOk);
+                    res.end();
+                }
+            );
+            //res.json(statusOk);
+        }else{
+            res.json(statusError);
+            res.end();
+        }
+    },
+    update : function(id, hospital, res, next){
+        console.log('Update existing hospital');
+        //validate hospital
+        if((hospital) && (hospital.name.length) && hospital.name.length > 0 && (hospital.address) && hospital.address.length > 0){
+            this.connectDB();
+            //hospitalModel.push(hospital);
+
+            hospitalModel.findByIdAndUpdate(id, hospital,
+                function (err, post) {
+                    if (err) return next(err);
+                    res.json(statusOk);
+                    res.end();
+                }
+            );
+
+        }else{
+            res.json(statusError);
+            res.end();
+        }
+
+    }
+}
+
+
+/* GET home page. */
+router.get('/addDummy', function(req, res, next) {
+    clsHospital.addDummy(res);
+});
+
+router.get('/getAll', function(req, res, next) {
+    clsHospital.getAll(res);
+});
+
+router.get('/get/:id', function(req, res, next) {
+    clsHospital.getById(req.params.id, res, next);
+});
+
+router.delete('/delete/:id', function(req, res, next) {
+    clsHospital.deleteById(req.params.id, res, next);
+});
+
+router.post('/add',function(req, res, next){
+    try{
+        console.log('In Add');
+        console.log(req.body);
+        clsHospital.add(req.body, res, next);
+    }
+    catch(ex){
+        console.log(ex);
+        res.json(statusError);
+        res.end();
+    }
+});
+
+router.put('/update/:id',function(req, res, next){
+    try{
+        console.log('In Update');
+        console.log(req.body);
+        clsHospital.update(req.params.id, req.body, res, next);
+    }
+    catch(ex){
+        console.log(ex);
+        res.json(statusError);
+        res.end();
+    }
+
+});
+module.exports = router;
---
+++ Server/app/routes/locations.js
@@ -0,0 +1,196 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+var locationModel = mongoose.model('Location', {name: String, address: String, hospitalId : String});
+var hospitalModel = mongoose.model('Hospital', {name: String, address: String});
+
+var dbServer = 'mongodb://localhost:27017/QAWeb';
+var statusOk = {status:'ok', detail :''};
+var statusError =  {status :'500', detail :''};
+
+
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose default connection open to ' + dbServer);
+});
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {
+    console.log('Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', function() {
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+    });
+});
+
+
+// CLASS
+var clsLocation  = {
+    connectDB : function(){
+        mongoose.connect(dbServer,function(error){
+            if(error){
+                console.log(error);
+            }
+        });
+    },
+    disconnectDB :function(){
+
+    },
+    responseHandler : function (err, obj){
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('saved successfully:', obj);
+        }
+    },
+    addDummy : function(res){
+        this.connectDB();
+        var location1 = new locationModel({name: 'Mumbai', address :'Maharashtra, India'});
+        var location2 = new locationModel({name: 'Pune', address :'Maharashtra, India'});
+
+        location1.save(function (err, data) {
+            clsLocation.responseHandler(err,data);
+        });
+        location2.save(function (err, data) {
+            clsLocation.responseHandler(err,data);
+        });
+        this.disconnectDB();
+        res.json(statusOk);
+        res.end();
+        //mongoose.connection.close();
+    },
+    getAll : function(res){
+        console.log('GetAll executed');
+        this.connectDB();
+        locationModel.find({},function(err,docs){
+            docs.forEach(function(item){
+                hospitalModel.find({_id : item.hospitalId},function(err,hospital){
+                    if((hospital) && hospital.length > 0){
+                        item.Hospital = hospital[0];
+                    }
+                });
+            });
+            res.json(docs);
+            res.end();
+        });
+    },
+    getById : function(id,res, next){
+        console.log('Get by Name executed');
+        this.connectDB();
+        locationModel.findById(id,
+            function (err, data) {
+                if (err) return next(err);
+                res.json(data);
+                res.end();
+            }
+        );
+    },
+    deleteById :function(id,res, next){
+        console.log('Delete by Name executed');
+        this.connectDB();
+        locationModel.findByIdAndRemove(id,
+            function (err, post) {
+                if (err) return next(err);
+                res.json(statusOk);
+                res.end();
+            }
+        );
+
+    },
+    add :function(location, res, next){
+        console.log('Add new Hospital');
+        if((location) && (location.name.length) && location.name.length > 0 && (location.address) && location.address.length > 0 && (location.hospitalId) && location.hospitalId.length > 0){
+            this.connectDB();
+            locationModel.create(location,
+                function (err, post) {
+                    if (err) return next(err);
+                    res.json(statusOk);
+                    res.end();
+                }
+            );
+            //res.json(statusOk);
+        }else{
+            res.json(statusError);
+            res.end();
+        }
+    },
+    update : function(id, location, res, next){
+        console.log('Update existing hospital');
+        //validate hospital
+        if((location) && (location.name.length) && location.name.length > 0 && (location.address) && location.address.length > 0 && (location.hospitalId) && location.hospitalId.length > 0){
+            this.connectDB();
+            //hospitalModel.push(hospital);
+
+            locationModel.findByIdAndUpdate(id, hospital,
+                function (err, post) {
+                    if (err) return next(err);
+                    res.json(statusOk);
+                    res.end();
+                }
+            );
+
+        }else{
+            res.json(statusError);
+            res.end();
+        }
+
+    }
+}
+
+
+/* GET home page. */
+router.get('/addDummy', function(req, res, next) {
+    clsLocation.addDummy(res);
+});
+
+router.get('/getAll', function(req, res, next) {
+    clsLocation.getAll(res);
+});
+
+router.get('/get/:id', function(req, res, next) {
+    clsLocation.getById(req.params.id, res, next);
+});
+
+router.delete('/delete/:id', function(req, res, next) {
+    clsLocation.deleteById(req.params.id, res, next);
+});
+
+router.post('/add',function(req, res, next){
+    try{
+        console.log('In Add');
+        console.log(req.body);
+        clsLocation.add(req.body, res, next);
+    }
+    catch(ex){
+        console.log(ex);
+        res.json(statusError);
+        res.end();
+    }
+});
+
+router.put('/update/:id',function(req, res, next){
+    try{
+        console.log('In Update');
+        console.log(req.body);
+        clsLocation.update(req.params.id, req.body, res, next);
+    }
+    catch(ex){
+        console.log(ex);
+        res.json(statusError);
+        res.end();
+    }
+
+});
+module.exports = router;
+
+
+

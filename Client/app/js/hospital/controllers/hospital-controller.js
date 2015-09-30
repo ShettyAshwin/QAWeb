dc84e41948e64a8b02c319ede5dc265cb7b372a1
Priyanka-Katare <katare.priyanka@gmail.com>
2015-09-30 05:17:37 -0700

    hospital

--- Client/app/js/hospital/controllers/hospital-controller.js
+++ Client/app/js/hospital/controllers/hospital-controller.js
@@ -1,41 +1,40 @@
 /**
  * Created by katarep on 9/28/15.
  */
-barcoApp.controller('HospitalController',['$scope', 'hospitalService',
-    function($scope, hospitalService){
-        $scope.getHospital = function(){
-            hospitalService.getHospitalList().then(function(obj){
-                $scope.hospitals = obj.reponseData;
-            });
-        };
-        $scope.getHospital();
-        $scope.AddHospital = function()
-        {
-            var objHospital = $scope.Hospital;
-            if($scope.Hospital._id ==null || $scope.Hospital._id ==0)
-            {
-            hospitalService.AddHospitalDetail(objHospital).then(function (response) {
-            $scope.HospitalId = response._id;
-            $scope.Hospital = null;
-            $scope.getHospital();
-            });
-            }else
-            {
-                hospitalService.UpdateHospitalDetail(objHospital).then(function (response) {
-                    $scope.HospitalId = response._id;
-
+    barcoApp.controller('HospitalController',['$scope', 'hospitalService',
+        function($scope, hospitalService){
+            $scope.getHospital = function(){
+                hospitalService.getHospitalList().then(function(obj){
+                    $scope.hospitals = obj.reponseData;
                 });
-            }
-        };
-        $scope.getHospitalById = function(id){
-            hospitalService.getHospitalById(id).then(function(obj){
-                $scope.Hospital = obj.reponseData;
-            });
-
-        };
-        $scope.DeleteHospital = function(id){
-            hospitalService.DeleteHospitalDetail(id).then(function(){
-            });
+            };
             $scope.getHospital();
-        };
-}]);
+            $scope.AddHospital = function()
+            {
+                var objHospital = $scope.Hospital;
+                if($scope.Hospital._id ==null || $scope.Hospital._id ==0)
+                    {
+                    hospitalService.AddHospitalDetail(objHospital).then(function (response) {
+                    $scope.HospitalId = response._id;
+                    $scope.Hospital = null;
+                    $scope.getHospital();
+                    });
+                }else
+                {
+                    hospitalService.UpdateHospitalDetail(objHospital).then(function (response) {
+                        $scope.HospitalId = response._id;
+                        $scope.getHospital();
+                    });
+                }
+            };
+            $scope.getHospitalById = function(id){
+                hospitalService.getHospitalById(id).then(function(obj){
+                    $scope.Hospital = obj.reponseData;
+                });
+            };
+            $scope.DeleteHospital = function(id){
+                hospitalService.DeleteHospitalDetail(id).then(function(){
+                    $scope.getHospital();
+                });
+            };
+    }]);

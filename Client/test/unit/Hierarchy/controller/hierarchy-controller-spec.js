674f4c8e2faf6f9ff720d6aab2be13e2752b2813
RaibhanS <Raibhan.sasane@gmail.com>
2015-10-01 18:00:16 +0530

    Hierarchy unit test

---
+++ Client/test/unit/Hierarchy/controller/hierarchy-controller-spec.js
@@ -0,0 +1,117 @@
+describe('hierarchy', function () {
+    describe('controller', function () {
+        describe('hierarchySpec', function () {
+            var fakeScope, fakeController, fakeRootScope, httpBackend, fakeHierarchyService, fakeLocationService;
+            beforeEach(module('barcoApp'));
+
+            beforeEach(inject(function ($rootScope, $q, $controller, $httpBackend, hierarchyService, locationService) {
+                fakeScope = $rootScope.$new();
+                fakeRootScope = $rootScope;
+                fakeController = $controller;
+                fakeHierarchyService = hierarchyService;
+                fakeLocationService = locationService;
+                fakeScope.hierarchies = [
+                    {"_id": 1, "name": "hierarchy1", "address": "abc"},
+                    {"_id": 2, "name": "hierarchy1", "address": "abc"}
+                ];
+                fakeScope.Hierarchy = {"_id": 1, "name": "hierarchy1", "address": "abc"};
+                fakeScope.locationList = [
+                    { "_id": 1, "name": "loc1", "address": "addr1", "hospitalId": "1" },
+                    { "_id": 2, "name": "loc2", "address": "addr2", "hospitalId": "2" }
+                ];
+
+                httpBackend = $httpBackend;
+                fakeScope.getHierarchy = function () {
+                };
+                fakeScope.AddHierarchy = function () {
+                };
+                fakeScope.getHierarchyById = function () {
+                };
+                $controller('hierarchyController', {
+                    $scope: fakeScope,
+                    hierarchyService: fakeHierarchyService,
+                    locationService: fakeLocationService
+                });
+            }));
+
+            it('Should test get hierarchy Method which is used for getting hierarchy list', function () {
+                var tempLocations = fakeScope.locationList
+
+                var tempHierarchies = fakeScope.hierarchies;
+                httpBackend.when('GET', angular.getAppSection('location').getAll).respond(tempLocations);
+                httpBackend.when('GET', angular.getAppSection('hierarchy').getAll).respond(tempHierarchies);
+                fakeHierarchyService.getHierarchyById().then(function (response) {
+                    expect(response.name).toBe(tempLocations.name);
+                });
+                fakeScope.getHierarchies();
+                httpBackend.flush();
+                expect(tempLocations.length).toBeGreaterThan(0);
+            });
+
+            it('Should test AddHierarchy Method which is used add hospital details',function(){
+                var tempObj = fakeScope.hierarchies;
+                fakeScope.Hierarchy= {"_id": 0, "name": "hierarchy1", "address": "abc"};
+
+                var tempResponse = { 'Success': true, 'Data': '', 'error': null, 'ErrorCode': status };
+                httpBackend.when('GET', angular.getAppSection('hierarchy').getAll).respond(tempObj);
+                httpBackend.whenPOST("http://localhost:3000/Hierarchies/add/").respond(200,{});
+                fakeHierarchyService.AddHierarchyDetail(fakeScope.Hierarchy).then(function (response) {
+                    expect(response.Success).toBe(true);
+                });
+                fakeScope.addHierarchy();
+                httpBackend.flush();
+            });
+            it('Should test AddHierarchy Method for Update',function(){
+                var tempObj = fakeScope.hierarchies;
+                fakeScope.Hierarchy= {"_id": 1, "name": "hierarchy1", "address": "abc"};
+
+                var tempResponse = { 'Success': true, 'Data': '', 'error': null, 'ErrorCode': status };
+                httpBackend.when('GET', angular.getAppSection('hierarchy').getAll).respond(tempObj);
+                httpBackend.whenPUT("http://localhost:3000/Hierarchies/update/1").respond(200,{});
+                fakeHierarchyService.UpdateHierarchyDetail(fakeScope.Hierarchy).then(function (response) {
+                    expect(response.Success).toBe(true);
+                });
+                fakeScope.addHierarchy();
+                httpBackend.flush();
+            });
+            it('Should test getHierarchyById Method which is used specific hierarchy detail by id', function () {
+                var tempObj = fakeScope.Hierarchy;
+
+                fakeHierarchyService.getHierarchyById(fakeScope.Hierarchy._id).then(function (response) {
+                    fakeScope.Hierarchy = response;
+                    expect(response.name).toBe(tempObj.name);
+                });
+
+                fakeScope.getHierarchyById(fakeScope.Hierarchy._id);
+
+                expect(tempObj._id).toBe(1);
+            });
+
+            it('Should test getHierarchyById Method which is used specific hierarchy detail by id', function () {
+                var tempObj = fakeScope.Hierarchy;
+
+                fakeHierarchyService.getHierarchyById(fakeScope.hierarchies._id).then(function (response) {
+                    expect(response.name).toBe(tempObj.name);
+                });
+
+                fakeScope.getHierarchyById(fakeScope.Hierarchy._id);
+
+                expect(tempObj._id).toBe(1);
+            });
+
+            it('Should test Deletehierarchy Method which is used delete specific hierarchy detail by id', function () {
+                var tempObj = fakeScope.locationList;
+                var hierarchyToDelete = '1';
+                var tempResponse = { 'Success': true, 'Data': '', 'error': null, 'ErrorCode': status };
+                httpBackend.when('GET', angular.getAppSection('hierarchy').getAll).respond(tempObj);
+                httpBackend.when("DELETE" ,"http://localhost:3000/Hierarchies/delete/1").respond(tempResponse);
+
+                fakeHierarchyService.DeleteHierarchyDetail(hierarchyToDelete).then(function (response) {
+                    expect(response.Success).toBe(true);
+                });
+                fakeScope.deleteHierarchy(hierarchyToDelete);
+                httpBackend.flush();
+            });
+        })
+    })
+})
---
+++ Client/test/unit/Hierarchy/service/hierarchy-service-spec.js
@@ -0,0 +1,105 @@
+/**
+ * Created by katarep on 9/29/15.
+ */
+describe('hospital',function(){
+    describe('services',function(){
+        describe('hospitalServiceSpec',function(){
+            var  fakeScope, fakehospitalService, httpBackend;
+
+
+            beforeEach(module('barcoApp'));
+
+
+            beforeEach(inject(function ($rootScope,$httpBackend,hospitalService) {
+                fakeScope = $rootScope.$new();
+                fakehospitalService = hospitalService;
+                fakeScope.hospitals= [{"_id":1,"name":"hospital1","address":"abc"},{"_id":2,"name":"hospital1","address":"abc"}];
+                fakeScope.Hospital= {"_id":1,"name":"hospital1","address":"abc"};
+                httpBackend = $httpBackend;
+
+            }));
+
+            it('Should test getHospitalList Method which is used for getting hospital list',function(){
+                var tempObj =  fakeScope.hospitals;
+
+                httpBackend.when('GET',angular.getAppSection('hospital').list).respond(tempObj);
+                fakehospitalService.GetHospitalList().then(function(response){
+                    expect(response.name).toBe(tempObj.name);
+                });
+                httpBackend.flush();
+                expect(tempObj.length).toBeGreaterThan(0);
+            });
+            it('Should test getHospitalById Method which is used for getting specific hospital details',function(){
+                var tempObj =  fakeScope.hospitals;
+                httpBackend.when('GET',angular.getAppSection('hospital').get+tempObj._id).respond(tempObj);
+                fakehospitalService.GetHospitalById(fakeScope.hospitals._id).then(function(response){
+                    expect(response.name).toBe(tempObj.name);
+                });
+                httpBackend.flush();
+            });
+
+            it('Should test AddHospitalDetail Method which is used add hospital details',function(){
+                var tempObj =  fakeScope.Hospital;
+                httpBackend.expectPOST(angular.getAppSection('hospital').add).respond(200,tempObj);
+                fakehospitalService.AddHospitalDetail().then(function(response){
+                    expect(response.Data._id).toBe(tempObj._id);
+                });
+                httpBackend.flush();
+
+            });
+
+            it('Should test AddHospitalDetail Method with error',function(){
+                var tempObj =  fakeScope.Hospital;
+                httpBackend.expectPOST(angular.getAppSection('hospital').add).respond(500,tempObj);
+                fakehospitalService.AddHospitalDetail().then(function(response){
+
+                    expect(response.Data).toBe(null);
+                });
+                httpBackend.flush();
+
+            });
+
+            it('Should test UpdateHospitalDetail Method which is used update hospital details',function(){
+                var tempObj =  fakeScope.Hospital;
+                tempObj.name = "hospital2";
+                httpBackend.expectPUT(angular.getAppSection('hospital').update+'1').respond(200,tempObj);
+                fakehospitalService.UpdateHospitalDetail(fakeScope.Hospital).then(function(response){
+                    expect(response.Data.name).toBe(tempObj.name);
+                });
+                httpBackend.flush();
+            });
+
+            it('Should test UpdateHospitalDetail Method with error',function(){
+                var tempObj =  fakeScope.Hospital;
+                tempObj.name = "hospital2";
+                httpBackend.expectPUT(angular.getAppSection('hospital').update+'1').respond(500,tempObj);
+                fakehospitalService.UpdateHospitalDetail(fakeScope.Hospital).then(function(response){
+                    expect(response.Data).toBe(null);
+                });
+                httpBackend.flush();
+            });
+
+            it('Should test DeleteHospitalDetail Method which is used delete hospital details',function(){
+                var tempObj =  fakeScope.Hospital;
+
+                httpBackend.expectDELETE(angular.getAppSection('hospital').delete +'1').respond(200);
+                fakehospitalService.DeleteHospitalDetail(1).then(function(){
+                    expect(true).toBe(true);
+                });
+                httpBackend.flush();
+
+            });
+
+            it('Should test DeleteHospitalDetail Method with error',function(){
+                var tempObj =  fakeScope.Hospital;
+
+                httpBackend.expectDELETE(angular.getAppSection('hospital').delete +'1').respond(500);
+                fakehospitalService.DeleteHospitalDetail(1).then(function(){
+                    expect(false).toBe(false);
+                });
+                httpBackend.flush();
+
+            });
+        });
+    });
+});

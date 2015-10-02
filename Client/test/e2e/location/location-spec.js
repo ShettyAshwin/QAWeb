28b01e732fd0141de2d51ef35b649d4d58f8f84f
prashant <prashantvj94@gmail.com>
2015-10-02 12:50:31 +0530

    Location E2E

--- Client/test/e2e/location/location-spec.js
+++ Client/test/e2e/location/location-spec.js
@@ -8,35 +8,35 @@ describe('Location Test', function () {
     url = '/QAWebPortal/app/index.html#/Hospitals';
 
     beforeEach(function () {
-        //pause();
         browser().navigateTo(url);
-        //pause();
         element('.container nav ul li:eq(1) a').click();
     });
 
-    it('should add location', function () {
-        pause();
+    it('should add new location', function () {
         element('#btnAddLocation').click(); // Open add location view
         input('Location.name').enter('testName');
         input('Location.address').enter('testAddress');
         select('Location.associatedHospital').option('ABC Hospital');
-        pause();
         element('#saveLocation').click();
-        alert(element('#location-table tbody tr').count());
-        pause();
         expect(repeater('#location-table tbody tr').count()).toBeGreaterThan(1);
     });
 
     it('should edit location', function () {
-        pause();
-        var name = element('#location-table tbody tr:last td:eq(0)').text();
+        var name = element('#location-table tr:last td:eq(0)').text();
+        name.execute(function () { });
         element('#location-table tbody tr:last .icnedit').click(); // Open edit location view
-        input('Location.name').enter(name + 'NEW');
-        pause();
+        input('Location.name').enter(name.value + 'NEW');
         element('#saveLocation').click();
-        alert(element('#location-table tbody tr').count());
-        pause();
         expect(repeater('#location-table tbody tr').count()).toBeGreaterThan(1);
     });
 
+    it('should delete location', function () {
+        var rowCount = element('#location-table tr').count();
+        rowCount.execute(function () { });
+        var beforeCount = rowCount.value;
+        element('#location-table tbody tr:last .icndelete').click(); // delete location
+        rowCount = element('#location-table tr').count();
+        rowCount.execute(function () { });
+        expect(rowCount).toBe(beforeCount - 1);
+    });
 });

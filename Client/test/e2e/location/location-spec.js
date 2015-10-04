95655affc77aa31a4e2c1416c367351cddd1a5b4
RaibhanS <Raibhan.sasane@gmail.com>
2015-10-04 23:35:06 +0530

    E2e Tests

--- Client/test/e2e/location/location-spec.js
+++ Client/test/e2e/location/location-spec.js
@@ -10,7 +10,6 @@ describe('Location Test', function () {
     beforeEach(function () {
         browser().navigateTo(url);
         element('.container nav ul li:eq(1) a').click();
-        pause();
     });
 
     it('should add new location', function () {
@@ -24,7 +23,8 @@ describe('Location Test', function () {
 
     it('should edit location', function () {
         var name = element('#location-table tr:last td:eq(0)').text();
-        name.execute(function () { });
+        name.execute(function () {
+        });
         element('#location-table tbody tr:last .icnedit').click(); // Open edit location view
         input('Location.name').enter(name.value + 'NEW');
         element('#saveLocation').click();
@@ -33,11 +33,21 @@ describe('Location Test', function () {
 
     it('should delete location', function () {
         var rowCount = element('#location-table tr').count();
-        rowCount.execute(function () { });
+        rowCount.execute(function () {
+        });
         var beforeCount = rowCount.value;
         element('#location-table tbody tr:last .icndelete').click(); // delete location
         rowCount = element('#location-table tr').count();
-        rowCount.execute(function () { });
+        rowCount.execute(function () {
+        });
         expect(rowCount).toBe(beforeCount - 1);
+    });
+    it('should add new location', function () {
+        element('#btnAddLocation').click(); // Open add location view
+        input('Location.name').enter('LocationForHierarchyTest');
+        input('Location.address').enter('testAddress');
+        select('Location.associatedHospital').option('ABC Hospital');
+        element('#saveLocation').click();
+        expect(repeater('#location-table tbody tr').count()).toBeGreaterThan(1);
     });
 });

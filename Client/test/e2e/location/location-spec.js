0c03bb93941e659bd6051320f23fb83e48205376
prashant <prashantvj94@gmail.com>
2015-10-05 15:06:13 +0530

    Location E2E bug fix

--- Client/test/e2e/location/location-spec.js
+++ Client/test/e2e/location/location-spec.js
@@ -18,31 +18,30 @@ describe('Location Test', function () {
         input('Location.address').enter('testAddress');
         select('Location.associatedHospital').option('ABC Hospital');
         element('#saveLocation').click();
-        expect(repeater('#location-table tbody tr').count()).toBeGreaterThan(1);
+        expect(repeater('#location-table tbody tr.validRecord').count()).toBeGreaterThan(0);
     });
 
     it('should edit location', function () {
-        var name = element('#location-table tr:last td:eq(0)').text();
-        name.execute(function () {
-        });
-        element('#location-table tbody tr:last .icnedit').click(); // Open edit location view
+        var name = element('#location-table tr.validRecord:last td:eq(0)').text();
+        name.execute(function () { });
+        element('#location-table tbody tr.validRecord:last .icnedit').click(); // Open edit location view
         input('Location.name').enter(name.value + 'NEW');
         element('#saveLocation').click();
-        expect(repeater('#location-table tbody tr').count()).toBeGreaterThan(1);
+        expect(repeater('#location-table tbody tr.validRecord').count()).toBeGreaterThan(0);
     });
 
     it('should delete location', function () {
-        var rowCount = element('#location-table tr').count();
-        rowCount.execute(function () {
-        });
+        var rowCount = element('#location-table tr.validRecord').count();
+        rowCount.execute(function () { });
         var beforeCount = rowCount.value;
-        element('#location-table tbody tr:last .icndelete').click(); // delete location
-        rowCount = element('#location-table tr').count();
+        element('#location-table tbody tr.validRecord:last .icndelete').click(); // delete location
+        rowCount = element('#location-table tr.validRecord').count();
         rowCount.execute(function () {
         });
         expect(rowCount).toBe(beforeCount - 1);
     });
-    it('should add new location', function () {
+
+    it('should add new location for first hierarchy', function () {
         element('#btnAddLocation').click(); // Open add location view
         input('Location.name').enter('LocationForHierarchyTest');
         input('Location.address').enter('testAddress');

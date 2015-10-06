15cb63c04174f958e374f645c1305431f7ea2676
RaibhanS <Raibhan.sasane@gmail.com>
2015-10-06 11:54:01 +0530

    Added removed files previously

--- Client/app/js/TreeView/directives/vertical-tree-view.js
+++ Client/app/js/TreeView/directives/vertical-tree-view.js
@@ -7,10 +7,14 @@
     f.module("angularTreeview", []).directive("treeModel", function ($compile) {
         return{restrict: "A", link: function (b, h, c) {
 
-            var a = c.treeId, g = c.treeModel, e = c.nodeLabel || "label", d = c.nodeChildren || "children",t= c.nodeType, e = '<ul>' +
-                '<li data-ng-repeat="node in ' + g + '"><i class="collapsed" data-ng-show="node.type !=asset && node.' + d + '.length && node.collapsed" data-ng-click="' + a + '.selectNodeHead(node)"></i>' +
-                '<i class="expanded" data-ng-show="node.type !=asset && node.' + d + '.length && !node.collapsed" data-ng-click="' + a + '.selectNodeHead(node)"></i>' +
-                '<i class="normal" data-ng-show="node.type == asset" ></i> ' +
+            var a = c.treeId, g = c.treeModel, e = c.nodeLabel || "label", d = c.nodeChildren || "children", t = c.nodeType, e = '<ul>' +
+
+                '<li data-ng-repeat="node in ' + g + '">' +
+
+                '<i ng-class="{{ node.type == asset ? normal :expanded}}" ' +
+               /* ' ( node.type !== asset && node.nodeChildren.length && !node.collapsed ? expanded ' +
+                ': collapsed)}}" ' +*/
+                'data-ng-click="' + a + '.selectNodeHead(node)"></i>' +
                 '<span data-ng-class="node.selected" data-ng-click="' + a + '.selectNodeLabel(node)">{{node.' + e + '}}</span>' +
                 '<span >{{node.type}}</span>' +
                 '<div data-ng-hide="node.collapsed" data-tree-id="' + a + '" data-tree-model="node.' + d + '" data-node-id=' + (c.nodeId || "id") + " data-node-label=" + e + " data-node-children=" + d + ">" +
@@ -28,3 +32,35 @@
         }}
     })
 })(angular);
+
+/*
+
+
+ (function (f) {
+ f.module("angularTreeview", []).directive("treeModel", function ($compile) {
+ return{restrict: "A", link: function (b, h, c) {
+
+ var a = c.treeId, g = c.treeModel, e = c.nodeLabel || "label", d = c.nodeChildren || "children",t= c.nodeType, e = '<ul>' +
+
+ '<li data-ng-repeat="node in ' + g + '">' +
+ '<i class="normal" data-ng-show={{node.type == asset}} ></i> ' +
+ '<i class="collapsed" data-ng-show={{node.type == asset && node.nodeChildren.length && node.collapsed}} data-ng-click="' + a + '.selectNodeHead(node)"></i>' +
+ '<i class="expanded" data-ng-show={{node.type == asset && node.nodeChildren.length && !node.collapsed}} data-ng-click="' + a + '.selectNodeHead(node)"></i>' +
+
+ '<span data-ng-class="node.selected" data-ng-click="' + a + '.selectNodeLabel(node)">{{node.' + e + '}}</span>' +
+ '<span >{{node.type}}</span>' +
+ '<div data-ng-hide="node.collapsed" data-tree-id="' + a + '" data-tree-model="node.' + d + '" data-node-id=' + (c.nodeId || "id") + " data-node-label=" + e + " data-node-children=" + d + ">" +
+ "</div>" +
+ "</li>" +
+ "</ul>";
+ a && g && (c.angularTreeview && (b[a] = b[a] || {}, b[a].selectNodeHead = b[a].selectNodeHead || function (a) {
+ a.collapsed = !a.collapsed
+ }, b[a].selectNodeLabel = b[a].selectNodeLabel || function (c) {
+ b[a].currentNode && b[a].currentNode.selected &&
+ (b[a].currentNode.selected = void 0);
+ c.selected = "selected";
+ b[a].currentNode = c
+ }), h.html('').append($compile(e)(b)))
+ }}
+ })
+ })(angular);*/
